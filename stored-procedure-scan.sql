e7f03167bcce6f718f87bba9e91d2734bd833f2a
Janus van Rensburg <janusvanrensburg@outlook.com>
2021-09-16 20:09:36 -0500

    Update stored-procedure-scan.sql

--- stored-procedure-scan.sql
+++ stored-procedure-scan.sql
@@ -1,8 +1,7 @@
-
 SELECT DISTINCT [OBJ].[name]
   FROM [sys].[sysobjects] AS [OBJ]
 
        INNER JOIN [sys].[syscomments] AS [TXT]
                ON [TXT].[id] = [OBJ].[id]
 
-WHERE [text] LIKE '%%'
+WHERE [text] LIKE '%%';
