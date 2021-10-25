f08b2db8e365cc5693957ed3a92431cefbdc723a
Janus van Rensburg <janusvanrensburg@outlook.com>
2021-10-25 11:18:42 -0500

    Updates

--- sql/object-definition-scan.sql
+++ sql/object-definition-scan.sql
@@ -1,11 +1,19 @@
 
-SELECT DISTINCT [OBJ].[name]
-  FROM [sys].[sysobjects] AS [OBJ]
+DECLARE @Search NVARCHAR(MAX) = 'LASTSETTLEDATE'
+
+SELECT DISTINCT [OBJ].[type_desc]   AS [Object_Type]
+      ,[SCH].[name]                 AS [Object_Schema]
+      ,[OBJ].[name]                 AS [Object_Name]
+
+  FROM [sys].[objects] AS [OBJ]
+
+       INNER JOIN [sys].[schemas] AS [SCH]
+               ON [SCH].[schema_id] = [OBJ].[schema_id]
 
        INNER JOIN [sys].[syscomments] AS [TXT]
-               ON [TXT].[id] = [OBJ].[id]
+               ON [TXT].[id] = [OBJ].[object_id]
 
-WHERE [text] LIKE '%%';
+ WHERE [TXT].[text] LIKE CONCAT('%', @Search, '%');
 
 GO
 
